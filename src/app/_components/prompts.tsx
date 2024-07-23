4968157bab5a1f4f46b4064319cf8aaeccba348c
Anish Anne <22670462+anishanne@users.noreply.github.com>
2024-07-23 10:44:02 -0400

    Team, Project, Prompt components

--- src/app/_components/prompts.tsx
+++ src/app/_components/prompts.tsx
@@ -3,16 +3,13 @@
 import { api } from "@/trpc/react";
 
 export function Prompts({ projectId }: { projectId: string }) {
-  const [prompts] = api.prompt.get.useSuspenseQuery({ projectId });
+  const [prompts] = api.prompt.list.useSuspenseQuery({ projectId });
 
   return (
     <div className="w-full max-w-xs">
       {prompts ? (
         <p className="truncate">
-          Your prompts:{" "}
-          {prompts.map((prompt) => (
-            <>{prompt.name}</>
-          ))}
+          Your prompts: {prompts.map((prompt) => prompt.name).join(", ")}
         </p>
       ) : (
         <p>You have no prompts yet.</p>
