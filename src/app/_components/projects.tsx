d941adb6b699005c6ac557ab4cffb21aa7c91677
Anish Anne <22670462+anishanne@users.noreply.github.com>
2024-07-23 11:47:14 -0400

    Add project + prompt components

--- src/app/_components/projects.tsx
+++ src/app/_components/projects.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { api } from "@/trpc/react";
+import Link from "next/link";
 
 export function Project({ teamId }: { teamId: string }) {
   const [projects] = api.project.list.useSuspenseQuery({ teamId });
@@ -9,7 +10,19 @@ export function Project({ teamId }: { teamId: string }) {
     <div className="w-full max-w-xs">
       {projects ? (
         <p className="truncate">
-          Your project lst: {projects.map((project) => project.name).join(", ")}
+          Your project lst:
+          {projects.map((project) => (
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
+              href={`/t/${teamId}/p/${project.id}`}
+              key={project.id}
+            >
+              <h3 className="text-2xl font-bold">{project.name} →</h3>
+              <div className="text-lg">
+                {project.prompts.length} prompts in this project.
+              </div>
+            </Link>
+          ))}
         </p>
       ) : (
         <p>You have no projects yet.</p>
